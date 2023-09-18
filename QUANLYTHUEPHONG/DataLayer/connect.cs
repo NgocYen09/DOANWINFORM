df56f219ad823c6d8a7f2fb5c23dd10e7a1ace6d
NgocYen09 <ngocyen1865@gmail.com>
2023-09-18 20:52:35 +0700

    V1

---
+++ QUANLYTHUEPHONG/DataLayer/connect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class connect
+    {
+        public string getConnect()
+        {
+            SqlConnectionStringBuilder sqlConnectBuiler = new SqlConnectionStringBuilder();
+            sqlConnectBuiler.DataSource = @"DESKTOP-MLT7I76\SQLEXPRESS";
+            sqlConnectBuiler.InitialCatalog = "HOTELS";
+            sqlConnectBuiler.PersistSecurityInfo = true;
+            sqlConnectBuiler.UserID = "sa";
+            sqlConnectBuiler.Password = "2802";
+            sqlConnectBuiler.MultipleActiveResultSets = true;
+            sqlConnectBuiler.ApplicationName = "EntityFramework";
+            EntityConnectionStringBuilder entityBuilder = new EntityConnectionStringBuilder();
+            entityBuilder.Provider = "System.Data.SqlClient";
+            entityBuilder.ProviderConnectionString = sqlConnectBuiler.ConnectionString;
+            entityBuilder.Metadata = @"res://*/KHACHSAN.csdl|res://*/KHACHSAN.ssdl|res://*/KHACHSAN.msl";
+
+            return entityBuilder.ConnectionString;
+        }
+    }
+}
